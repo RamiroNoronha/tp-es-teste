d3ec552208ce748c080926e3ccf4779057fcf4f5
Ramiro Noronha <ramironrribeiro@gmail.com>
2024-12-14 17:39:31 -0300

    teste rule set

--- src/controllers/__test__/userController.test.ts
+++ src/controllers/__test__/userController.test.ts
@@ -33,144 +33,144 @@ describe('User Controller', () => {
         expect(response.body).toEqual(mockUsers);
     });
 
-    it('should return 500 when users routes is called if there is an error', async () => {
-        (pool.query as jest.Mock).mockRejectedValueOnce({
-            message: 'Database error',
-        } as never);
+    // it('should return 500 when users routes is called if there is an error', async () => {
+    //     (pool.query as jest.Mock).mockRejectedValueOnce({
+    //         message: 'Database error',
+    //     } as never);
 
-        const response = await request(app).get('/api/users');
+    //     const response = await request(app).get('/api/users');
 
-        expect(response.status).toBe(500);
-        expect(response.body).toEqual({ error: 'Database error' });
-    });
+    //     expect(response.status).toBe(500);
+    //     expect(response.body).toEqual({ error: 'Database error' });
+    // });
 
-    it('should create a new user', async () => {
-        const newUser = { username: 'user2', password: 'pass2' };
-        (pool.query as jest.Mock).mockReturnValue([{ insertId: 2 }]);
+    // it('should create a new user', async () => {
+    //     const newUser = { username: 'user2', password: 'pass2' };
+    //     (pool.query as jest.Mock).mockReturnValue([{ insertId: 2 }]);
 
-        const response = await request(app)
-            .post('/api/users')
-            .send(newUser);
+    //     const response = await request(app)
+    //         .post('/api/users')
+    //         .send(newUser);
 
-        expect(response.status).toBe(201);
-        expect(response.body).toEqual({ id: 2 });
-    });
+    //     expect(response.status).toBe(201);
+    //     expect(response.body).toEqual({ id: 2 });
+    // });
 
-    it('should return status code 400 when username is undefined in the post route users', async () => {
-        const newUser = { password: 'pass2' };
+    // it('should return status code 400 when username is undefined in the post route users', async () => {
+    //     const newUser = { password: 'pass2' };
 
-        const response = await request(app)
-            .post('/api/users')
-            .send(newUser);
+    //     const response = await request(app)
+    //         .post('/api/users')
+    //         .send(newUser);
 
-        expect(response.status).toBe(400);
-        expect(response.body).toEqual({ error: 'Invalid request' });
-    });
+    //     expect(response.status).toBe(400);
+    //     expect(response.body).toEqual({ error: 'Invalid request' });
+    // });
 
-    it('should return status code 400 when password is undefined in the post route users', async () => {
-        const newUser = { username: 'user2', };
+    // it('should return status code 400 when password is undefined in the post route users', async () => {
+    //     const newUser = { username: 'user2', };
 
-        const response = await request(app)
-            .post('/api/users')
-            .send(newUser);
+    //     const response = await request(app)
+    //         .post('/api/users')
+    //         .send(newUser);
 
-        expect(response.status).toBe(400);
-        expect(response.body).toEqual({ error: 'Invalid request' });
-    });
+    //     expect(response.status).toBe(400);
+    //     expect(response.body).toEqual({ error: 'Invalid request' });
+    // });
 
-    it('should return 500 when users post routes is called if there is an error', async () => {
-        const newUser = { username: 'user2', password: 'pass2' };
+    // it('should return 500 when users post routes is called if there is an error', async () => {
+    //     const newUser = { username: 'user2', password: 'pass2' };
 
-        (pool.query as jest.Mock).mockRejectedValueOnce({
-            message: 'Database error',
-        } as never);
+    //     (pool.query as jest.Mock).mockRejectedValueOnce({
+    //         message: 'Database error',
+    //     } as never);
 
-        const response = await request(app).post('/api/users').send(newUser);
+    //     const response = await request(app).post('/api/users').send(newUser);
 
-        expect(response.status).toBe(500);
-        expect(response.body).toEqual({ error: 'Database error' });
-    });
+    //     expect(response.status).toBe(500);
+    //     expect(response.body).toEqual({ error: 'Database error' });
+    // });
 
-    it('should get a specific user when call getUserById with a existet user', async () => {
-        const mockUsers = [{ id: 1, username: 'user1', password: 'pass1' }];
-        (pool.query as jest.Mock).mockReturnValue([mockUsers]);
+    // it('should get a specific user when call getUserById with a existet user', async () => {
+    //     const mockUsers = [{ id: 1, username: 'user1', password: 'pass1' }];
+    //     (pool.query as jest.Mock).mockReturnValue([mockUsers]);
 
-        const userId = 1;
-        const response = await request(app).get(`/api/users/${userId}`);
+    //     const userId = 1;
+    //     const response = await request(app).get(`/api/users/${userId}`);
 
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual(mockUsers[0]);
-    });
+    //     expect(response.status).toBe(200);
+    //     expect(response.body).toEqual(mockUsers[0]);
+    // });
 
-    it('should return status code 404 when the user is not founded', async () => {
-        (pool.query as jest.Mock).mockReturnValue([[]]);
+    // it('should return status code 404 when the user is not founded', async () => {
+    //     (pool.query as jest.Mock).mockReturnValue([[]]);
 
-        const userId = 1;
-        const response = await request(app).get(`/api/users/${userId}`);
+    //     const userId = 1;
+    //     const response = await request(app).get(`/api/users/${userId}`);
 
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'User not found' });
-    });
+    //     expect(response.status).toBe(404);
+    //     expect(response.body).toEqual({ error: 'User not found' });
+    // });
 
-    it('should return status code 200 when affectedRows is bigger then 0', async () => {
-        const updatedUser = { username: 'user2', password: 'pass2' };
-        const userId = 1;
-        (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 1 }]);
+    // it('should return status code 200 when affectedRows is bigger then 0', async () => {
+    //     const updatedUser = { username: 'user2', password: 'pass2' };
+    //     const userId = 1;
+    //     (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 1 }]);
 
-        const response = await request(app)
-            .put(`/api/users/${userId}`)
-            .send(updatedUser);
+    //     const response = await request(app)
+    //         .put(`/api/users/${userId}`)
+    //         .send(updatedUser);
 
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({ message: 'User updated successfully' });
-    });
+    //     expect(response.status).toBe(200);
+    //     expect(response.body).toEqual({ message: 'User updated successfully' });
+    // });
 
-    it('should return status code 404 when has no affectedRows', async () => {
-        const updatedUser = { username: 'user2', password: 'pass2' };
-        const userId = 1;
-        (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 0 }]);
+    // it('should return status code 404 when has no affectedRows', async () => {
+    //     const updatedUser = { username: 'user2', password: 'pass2' };
+    //     const userId = 1;
+    //     (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 0 }]);
 
-        const response = await request(app)
-            .put(`/api/users/${userId}`)
-            .send(updatedUser);
+    //     const response = await request(app)
+    //         .put(`/api/users/${userId}`)
+    //         .send(updatedUser);
 
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'User not found' });
-    });
+    //     expect(response.status).toBe(404);
+    //     expect(response.body).toEqual({ error: 'User not found' });
+    // });
 
-    it('should return status code 400 when has no username neither password', async () => {
-        const updatedUser = {};
-        const userId = 1;
+    // it('should return status code 400 when has no username neither password', async () => {
+    //     const updatedUser = {};
+    //     const userId = 1;
 
-        const response = await request(app)
-            .put(`/api/users/${userId}`)
-            .send(updatedUser);
+    //     const response = await request(app)
+    //         .put(`/api/users/${userId}`)
+    //         .send(updatedUser);
 
-        expect(response.status).toBe(400);
-        expect(response.body).toEqual({ error: 'Invalid request' });
-    });
+    //     expect(response.status).toBe(400);
+    //     expect(response.body).toEqual({ error: 'Invalid request' });
+    // });
 
-    it('should return status code 200 when has one affectedRows when deleting a user', async () => {
-        const userId = 1;
-        (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 1 }]);
+    // it('should return status code 200 when has one affectedRows when deleting a user', async () => {
+    //     const userId = 1;
+    //     (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 1 }]);
 
-        const response = await request(app)
-            .delete(`/api/users/${userId}`);
+    //     const response = await request(app)
+    //         .delete(`/api/users/${userId}`);
 
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({ message: 'User deleted successfully' });
-    });
+    //     expect(response.status).toBe(200);
+    //     expect(response.body).toEqual({ message: 'User deleted successfully' });
+    // });
 
-    it('should return status code 404 when has no affectedRows when deleting a user', async () => {
-        const userId = 1;
-        (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 0 }]);
+    // it('should return status code 404 when has no affectedRows when deleting a user', async () => {
+    //     const userId = 1;
+    //     (pool.query as jest.Mock).mockReturnValue([{ affectedRows: 0 }]);
 
-        const response = await request(app)
-            .delete(`/api/users/${userId}`);
+    //     const response = await request(app)
+    //         .delete(`/api/users/${userId}`);
 
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ error: 'User not found' });
-    });
+    //     expect(response.status).toBe(404);
+    //     expect(response.body).toEqual({ error: 'User not found' });
+    // });
 
 
 });
