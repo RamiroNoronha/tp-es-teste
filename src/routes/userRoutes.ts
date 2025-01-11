ae89efacf8e75bc8b16056c881774739b80d62fd
andreb587~ <andreadesbarros@gmail.com>
2025-01-11 19:44:33 -0300

    Implementação inicial da UI - Tela de Login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,18 +1,18 @@
 import { Router } from 'express';
-import { getUsers, createUser, getUserById, updateUser, deleteUser } from '../controllers/userController';
+import { getUsers, createUser, getUserById, updateUser, deleteUser, loginUser } from '../controllers/userController';
 import { DataSource } from 'typeorm';
 
-
 const createUserRoutes = (dataSource: DataSource) => {
-    const router = Router();
+  const router = Router();
 
-    router.get('/users', getUsers(dataSource));
-    router.post('/users', createUser(dataSource));
-    router.get('/users/:id', getUserById(dataSource));
-    router.put('/users/:id', updateUser(dataSource));
-    router.delete('/users/:id', deleteUser(dataSource));
+  router.get('/users', getUsers(dataSource));
+  router.post('/users', createUser(dataSource));
+  router.get('/users/:id', getUserById(dataSource));
+  router.put('/users/:id', updateUser(dataSource));
+  router.delete('/users/:id', deleteUser(dataSource));
+  router.post('/login', loginUser(dataSource));
 
-    return router;
+  return router;
 };
 
 export default createUserRoutes;
